dd14fb89474e33dc1726d9e9954bf0a14bb78f1a
kadoshita <sublimer.me@gmail.com>
2019-12-15 16:22:34 +0900

    Modelを追加

---
+++ Models/SampleData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NETCoreMySQLSample.Models
+{
+    public class SampleData
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+    }
+}
