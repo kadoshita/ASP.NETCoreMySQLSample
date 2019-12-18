90aba0a30d198ad4bc28c677121f9759255c306d
kadoshita <sublimer.me@gmail.com>
2019-12-18 20:58:37 +0900

    WebAppsデプロイの対応

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,9 +17,12 @@ namespace ASP.NETCoreMySQLSample
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -28,12 +32,31 @@ namespace ASP.NETCoreMySQLSample
         {
             services.AddRazorPages();
 
-            services.AddDbContext<UserContext>(options => options.UseMySql(Configuration.GetConnectionString("UserContext")));
+            if (_env.IsDevelopment())
+            {
+                services.AddDbContext<UserContext>(options => options.UseMySql(Configuration.GetConnectionString("UserContext")));
+            }
+            else
+            {
+                String connectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb");
+                Regex reg = new Regex(@"Database=(?<database>.*?);Data Source=(?<address>.*?):(?<port>.*?);User Id=(?<user>.*?);Password=(?<password>.*?)$", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                Match match = reg.Match(connectionString);
+                connectionString = $"server={match.Groups["address"].Value};port={match.Groups["port"].Value};user={match.Groups["user"].Value};password={match.Groups["password"].Value};Database={match.Groups["database"].Value}";
+                services.AddDbContext<UserContext>(options => options.UseMySql(connectionString));
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<UserContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
